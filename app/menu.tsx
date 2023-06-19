62df01c72cd848ac3684c131e2249b37e301a9ce
May Sunktong <pattaranan.sunktong@gmail.com>
2023-06-20 00:04:02 +0200

    first commit

---
+++ app/menu.tsx
@@ -0,0 +1,129 @@
+export const Menu = {
+  matcha_cookies: [
+    {
+      name: "Matcha Cookies with Chocolate Chips",
+      price: 65,
+      "gluten-free": false,
+    },
+    {
+      name: "Matcha Cookies with White Chocolate Chips",
+      price: 65,
+      "gluten-free": false,
+    },
+    {
+      name: "Matcha Cookies with Caramel Bits",
+      price: 65,
+      "gluten-free": false,
+    },
+    {
+      name: "Sakura Cookies",
+      price: 65,
+      "gluten-free": false,
+    },
+    {
+      name: "Dark Chocolate Cookies",
+      price: 65,
+      "gluten-free": false,
+    },
+    {
+      name: "Chai Tea Cookies",
+      price: 65,
+      "gluten-free": false,
+    },
+  ],
+  colded_latte: [
+    {
+      name: "Matcha Cold Whisk Latte",
+      price: 65,
+      origin: "Japan",
+      types_of_liquids: ["Lactose-free", "Soy", "Almond"],
+
+    },
+    {
+      name: "Chai Latte",
+      price: 65,
+      origin: "India",
+      types_of_liquids: ["Lactose-free", "Soy", "Almond"],
+    },
+    {
+      name: "Local Brew Latte",
+      price: 65,
+      origin: "Thailand",
+      types_of_liquids: ["Lactose-free", "Soy", "Almond"]
+    },
+  ],
+  flavored_matcha: [
+    {
+      name: "Matcha with Coconut Water",
+      price: 65,
+      breed: "Samidori",
+      quality: {
+        ceremonial_grade: true,
+        culinary_grade: false,
+      },
+    },
+    {
+      name: "Matcha Soda with Lavender Syrup",
+      price: 65,
+      breed: "Uji",
+      quality: {
+        ceremonial_grade: true,
+        culinary_grade: false,
+      },
+    },
+    {
+      name: "Matcha Soda with Rose Syrup",
+      price: 65,
+      breed: "Yama",
+      quality: {
+        ceremonial_grade: true,
+        culinary_grade: true,
+      },
+    },
+    {
+      name: "Matcha Soda with Peach Syrup",
+      price: 65,
+      breed: "Hikari",
+      quality: {
+        ceremonial_grade: false,
+        culinary_grade: true,
+      },
+    },
+    {
+      name: "Matcha Soda with Strawberry Syrup",
+      price: 65,
+      breed: "Kaori",
+      quality: {
+        ceremonial_grade: false,
+        culinary_grade: true,
+      },
+    },
+    {
+      name: "Matcha Soda with Lemon Flavor",
+      price: 65,
+      breed: "Kaori",
+      quality: {
+        ceremonial_grade: false,
+        culinary_grade: true,
+      },
+    }
+  ],
+  "soda_cans": [
+    {
+      name: "Uji Soda + Lemon",
+      price: 25,
+    },
+    {
+      name: "Uji Soda + Peach",
+      price: 25,
+    },
+    {
+      name: "Uji Soda + Rose",
+      price: 25,
+    },
+    {
+      name: "Uji Soda + Yuzu",
+      price: 25,
+    }
+  ]
+};
